e2a32315a91c0400eabc2ec1b853be6ab11166f9
Eero28 <eero.kantonen11@gmail.com>
2024-11-16 13:33:48 +0200

    reviewDetails styling and adding more data

--- ReviewApp/pages/ReviewDetails.tsx
+++ ReviewApp/pages/ReviewDetails.tsx
@@ -1,19 +1,53 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
-import React from 'react';
+import React, { FC } from 'react';
+import StarRating from 'react-native-star-rating-widget';
+interface ReviewItem {
+  reviewname: string;
+  imageUrl: string;
+  reviewDescription: string;
+  reviewRating: number;
+  createdAt: string;
+}
+interface ReviewDetailsProps {
+  route: {
+    params: {
+      item: ReviewItem;
+    };
+  };
+}
+const ReviewDetails: FC<ReviewDetailsProps> = ({ route }) => {
+  const { item } = route.params;
+  const doNothing = (): void => { };
 
-const ReviewDetails = ({ route }) => {
-  const { item } = route.params; 
-
+  const convertDate = (time:string) =>{
+    const date = new Date(time);
+    const options: Intl.DateTimeFormatOptions = { 
+      year: 'numeric', 
+      month: '2-digit', 
+      day: '2-digit', 
+      timeZone: 'Europe/Helsinki' 
+    };
+    const finlandDate = date.toLocaleDateString('en-FI', options);
+    return finlandDate
+  }
   return (
-    <View>
-      <Text>Review Details</Text>
-      <Text>ID: {item.id_review}</Text>
-      <Image
-                style={styles.image}
-                source={{
-                    uri: item.imageUrl,
-                }}
-            />
+    <View style={styles.container}>
+      <View style={styles.cardContainer}>
+        <Text style={styles.title}>{item.reviewname}</Text>
+        <Image
+          style={styles.image}
+          source={{
+            uri: item.imageUrl,
+          }}
+        />
+        <StarRating
+          rating={item.reviewRating}
+          onChange={doNothing}
+          color='#0f3c85'
+        />
+        <Text style={styles.text}>{item.reviewDescription}</Text>
+        <Text style={styles.text}>Reviewed: {convertDate(item.createdAt)}</Text>
+      </View>
     </View>
   );
 };
@@ -21,8 +55,33 @@ const ReviewDetails = ({ route }) => {
 export default ReviewDetails;
 
 const styles = StyleSheet.create({
-    image:{
-        height: 200,
-        width: 200
-    }
+  image: {
+    height: 300,
+    width: 300,
+    borderRadius: 10,
+    marginVertical: 10,
+  },
+  container: {
+    flex: 1,
+    paddingTop: 40,
+    alignItems: "center",
+    backgroundColor: "gray",
+  },
+  cardContainer: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 15,
+    alignItems: "center",
+    width: "90%",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  text: {
+    fontSize: 16,
+    color: "#333",
+    marginBottom: 10,
+  },
 });
--- ReviewApp/pages/ReviewsScreen.tsx
+++ ReviewApp/pages/ReviewsScreen.tsx
@@ -2,15 +2,9 @@ import { StyleSheet, SafeAreaView, FlatList } from 'react-native';
 import React, { FC } from 'react';
 import { useAuth } from '../ContexApi';
 import ReviewItem from '../components/ReviewItem';
-import { useNavigation } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-
-
 
 const ReviewsScreen: FC = () => {
   const { userReviews } = useAuth();
-  const navigation = useNavigation<any>();
-
   return (
     <SafeAreaView>
       <FlatList
