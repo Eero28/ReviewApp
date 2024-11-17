f83b44e03ebbd5aa3caca95039e5e827f0ae8cd2
Eero28 <eero.kantonen11@gmail.com>
2024-11-17 13:34:20 +0200

    add icons to tabnavigator

--- ReviewApp/components/TabNavigator.tsx
+++ ReviewApp/components/TabNavigator.tsx
@@ -5,19 +5,38 @@ import ReviewsScreen from '../pages/ReviewsScreen';
 import TakeImageScreen from '../pages/TakeImageScreen';
 import { useAuth } from '../ContexApi';
 import LoginScreen from '../pages/LoginScreen';
-
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 type Props = {}
 const Tab = createBottomTabNavigator();
 
-const TabNavigator = ({}) => {
-  const {userInfo} = useAuth()
-  if(!userInfo){
-    return <LoginScreen/>
+const TabNavigator = ({ }) => {
+  const { userInfo } = useAuth()
+  if (!userInfo) {
+    return <LoginScreen />
   }
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Reviews" component={ReviewsScreen} options={{headerShown: false}}/>
-      <Tab.Screen name="Take Image" component={TakeImageScreen} options={{headerShown: false}} />
+      <Tab.Screen
+        name="Reviews"
+        component={ReviewsScreen}
+        options={{
+          headerShown: false,  
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="reviews" size={size} color={color} /> 
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Take Image"
+        component={TakeImageScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="camera" size={24} color="black" />
+          ),
+        }}
+      />
     </Tab.Navigator>
   )
 }
