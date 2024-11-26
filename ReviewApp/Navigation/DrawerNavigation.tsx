d873da2f081b45963495c7ae9cb9924d7fd11a2e
Eero28 <eero.kantonen11@gmail.com>
2024-11-26 16:54:59 +0200

    Navigation remove allreviews from drawer and add name property to be more correct

--- ReviewApp/Navigation/DrawerNavigation.tsx
+++ ReviewApp/Navigation/DrawerNavigation.tsx
@@ -4,30 +4,28 @@ import BottomTabNavigator from './BottomTabNavigator';
 import ProfileScreen from '../pages/ProfileScreen';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { useAuth } from '../ContexApi';
-import AllReviews from '../pages/AllReviews';
-import Ionicons from '@expo/vector-icons/Ionicons';
+
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
   const { userInfo } = useAuth();
-  
   return (
     <Drawer.Navigator
       screenOptions={{
         drawerStyle: {
-          backgroundColor: '#003366', 
+          backgroundColor: '#003366',
         },
         drawerActiveTintColor: '#6200EE',
         drawerInactiveTintColor: '#B0B0B0',
         headerStyle: {
           backgroundColor: '#003366'
         },
-        headerTintColor: '#ffffff' 
+        headerTintColor: '#ffffff'
       }}
     >
       <Drawer.Screen
-        name="Home"
+        name={userInfo?.username ? "Reviews" : "Welcome"}
         component={BottomTabNavigator}
         options={{
           drawerIcon: ({ color }) => (
@@ -36,26 +34,15 @@ const DrawerNavigator = () => {
         }}
       />
       {userInfo && (
-        <>
-          <Drawer.Screen
-            name="All reviews"
-            component={AllReviews}
-            options={{
-              drawerIcon: ({ color }) => (
-                <Ionicons name="people-sharp" size={20} color={color} />
-              ),
-            }}
-          />
-          <Drawer.Screen
-            name="Profile"
-            component={ProfileScreen}
-            options={{
-              drawerIcon: ({ color }) => (
-                <MaterialIcons name="account-circle" size={20} color={color} />
-              ),
-            }}
-          />
-        </>
+        <Drawer.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            drawerIcon: ({ color }) => (
+              <MaterialIcons name="account-circle" size={20} color={color} />
+            ),
+          }}
+        />
       )}
     </Drawer.Navigator>
   );
