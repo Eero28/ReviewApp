1251a053b0eee7dede2981454eda8492306d6514
Eero28 <eero.kantonen11@gmail.com>
2025-08-27 13:17:10 +0300

    correct hot beverages value, so it comes when clicking the filter button

--- ReviewApp/helpers/categories.ts
+++ ReviewApp/helpers/categories.ts
@@ -1,9 +1,9 @@
 export const categories = [
-    { label: 'All', value: undefined, icon: 'all' },
-    { label: 'Beers', value: 'beer', icon: 'beer' },
-    { label: 'Wines', value: 'wine', icon: 'wine' },
-    { label: 'Softdrinks', value: 'softdrink', icon: 'softdrink' },
-    { label: 'Hot beverages', value: 'hot beverage', icon: 'hotbeverage' },
-    { label: 'Cocktails', value: 'cocktail', icon: "cocktail" },
-    { label: 'Others', value: 'other', icon: "other" }
+  { label: "All", value: undefined, icon: "all" },
+  { label: "Beers", value: "beer", icon: "beer" },
+  { label: "Wines", value: "wine", icon: "wine" },
+  { label: "Softdrinks", value: "softdrink", icon: "softdrink" },
+  { label: "Hot beverages", value: "hotbeverage", icon: "hotbeverage" },
+  { label: "Cocktails", value: "cocktail", icon: "cocktail" },
+  { label: "Others", value: "other", icon: "other" },
 ];
